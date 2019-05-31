58f27d509b534a50e1b42472ca285d6be1b087e8
Fidler <matthew.fidler@gmail.com>
2019-05-30 19:12:19 -0500

    Update documentation

--- R/dummy.R
+++ R/dummy.R
@@ -1,6 +1,18 @@
 ##' Dummy Functions
 ##'
 ##'@export
-dummyFunction <-function(){
-    # I don't do anything.
+RxODE.doc <- function(){
+    devtools::build_vignettes(quiet=FALSE)
+    if (file.exists("../RxODE/_pkgdown.yml")) {
+        try(unlink("_pkgdown.yml"));
+        file.copy("../RxODE/_pkgdown.yml", "_pkgdown.yml");
+    }
+    pkgdown::build_site()
+    writeLines(c("<!DOCTYPE html>",
+                 "<head>",
+                 "<meta http-equiv=\"Refresh\" content=\"0; url=https://nlmixrdevelopment.github.io/RxODE/index.html\" />",
+                 "</head>",
+                 "<html>",
+                 "</html>"),"docs/index.html")
+
 }
